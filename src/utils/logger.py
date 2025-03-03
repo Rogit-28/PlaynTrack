ea5e6bff9e6391cae7a83767eeeaf043ae1bc136
Rogit-28 <rogitsudharsan81@gmail.com>
2025-03-03 15:28:55 +0530

    Add logger utility for structured logging output

---
+++ src/utils/logger.py
@@ -0,0 +1,14 @@
+import logging
+
+
+def setup_logger(name: str = "playntrack", level: int = logging.INFO) -> logging.Logger:
+    logger = logging.getLogger(name)
+    if logger.handlers:
+        return logger
+
+    logger.setLevel(level)
+    handler = logging.StreamHandler()
+    formatter = logging.Formatter("%(asctime)s [%(levelname)s] %(message)s")
+    handler.setFormatter(formatter)
+    logger.addHandler(handler)
+    return logger
